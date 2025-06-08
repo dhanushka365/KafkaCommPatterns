218892a3c4d07bf368a0a38dfbf112c2b69833f5
dhanushka365 <66137046+dhanushka365@users.noreply.github.com>
2025-06-08 12:32:56 +0530

    Create Event classed added

---
+++ COMMON/Messaging/Events/Sample/Create/CompletedCreateSampleEvent.cs
@@ -0,0 +1,14 @@
+﻿namespace COMMON.Messaging.Events.Sample.Create
+{
+    public class CompletedCreateSampleEvent
+    {
+        public string? Id { get; set; }
+        public string? Name { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public string? Type { get; set; }
+
+        public int? Count { get; set; }
+    }
+}
---
+++ COMMON/Messaging/Events/Sample/Create/WantsCreateSampleEvent.cs
@@ -0,0 +1,13 @@
+﻿namespace COMMON.Messaging.Events.Sample.Create
+{
+    public class WantsCreateSampleEvent
+    {
+        public string? Name { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public string? Type { get; set; }
+
+        public int? Count { get; set; }
+    }
+}
